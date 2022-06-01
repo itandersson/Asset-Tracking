322449761b4d7f7c749ecfe2258cf61041a932a7
Joakim Andersson <joakim@ktv-sjobo.se>
2022-06-01 12:49:42 +0200

    Moves to new folder

---
+++ Model/Computers.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace Mini_Project_2.Model
+{
+    public abstract class Computers : Assets
+    {
+        public override string ToString()
+        {
+            string outPut = type.PadRight(10) + brand.PadRight(10) + model.PadRight(15) +
+                office.PadRight(10) + purchaseDate.ToString("yy/MM/dd").PadRight(16) +
+                price.ToString().PadRight(15) + currency.PadRight(10);
+
+            return outPut;
+        }
+    }
+
+    public class MacBook : Computers
+    {
+        public MacBook(DateTime purchaseDate, int price, string type, string brand, string model, string office, string currency)
+        {
+            base.purchaseDate = purchaseDate;
+            base.price = price;
+            base.type = type;
+            base.brand = brand;
+            base.model = model;
+            base.office = office;
+            base.currency = currency;
+        }
+    }
+
+    public class Asus : Computers
+    {
+
+        public Asus(DateTime purchaseDate, int price, string type, string brand, string model, string office, string currency)
+        {
+            base.purchaseDate = purchaseDate;
+            base.price = price;
+            base.type = type;
+            base.brand = brand;
+            base.model = model;
+            base.office = office;
+            base.currency = currency;
+        }
+    }
+
+    public class Lenovo : Computers
+    {
+
+        public Lenovo(DateTime purchaseDate, int price, string type, string brand, string model, string office, string currency)
+        {
+            base.purchaseDate = purchaseDate;
+            base.price = price;
+            base.type = type;
+            base.brand = brand;
+            base.model = model;
+            base.office = office;
+            base.currency = currency;
+        }
+    }
+
+    public class HP : Computers
+    {
+
+        public HP(DateTime purchaseDate, int price, string type, string brand, string model, string office, string currency)
+        {
+            base.purchaseDate = purchaseDate;
+            base.price = price;
+            base.type = type;
+            base.brand = brand;
+            base.model = model;
+            base.office = office;
+            base.currency = currency;
+        }
+    }
+}
---
+++ Model/MobilePhones.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace Mini_Project_2.Model
+{
+
+    public abstract class MobilePhones : Assets
+    {
+        public override string ToString()
+        {
+            string outPut = type.PadRight(10) + brand.PadRight(10) + model.PadRight(15) +
+                office.PadRight(10) + purchaseDate.ToString("yy/MM/dd").PadRight(16) +
+                price.ToString().PadRight(15) + currency.PadRight(10);
+
+            return outPut;
+        }
+    }
+
+    public class Iphone : MobilePhones
+    {
+
+        public Iphone(DateTime purchaseDate, int price, string type, string brand, string model, string office, string currency)
+        {
+            base.purchaseDate = purchaseDate;
+            base.price = price;
+            base.type = type;
+            base.brand = brand;
+            base.model = model;
+            base.office = office;
+            base.currency = currency;
+        }
+    }
+
+    public class Samsung : MobilePhones
+    {
+
+        public Samsung(DateTime purchaseDate, int price, string type, string brand, string model, string office, string currency)
+        {
+            base.purchaseDate = purchaseDate;
+            base.price = price;
+            base.type = type;
+            base.brand = brand;
+            base.model = model;
+            this.office = office;
+            base.currency = currency;
+        }
+    }
+
+    public class Nokia : MobilePhones
+    {
+
+        public Nokia(DateTime purchaseDate, int price, string type, string brand, string model, string office, string currency)
+        {
+            base.purchaseDate = purchaseDate;
+            base.price = price;
+            base.type = type;
+            base.brand = brand;
+            base.model = model;
+            base.office = office;
+            base.currency = currency;
+        }
+    }
+
+    public class Motorola : MobilePhones
+    {
+
+        public Motorola(DateTime purchaseDate, int price, string type, string brand, string model, string office, string currency)
+        {
+            base.purchaseDate = purchaseDate;
+            base.price = price;
+            base.type = type;
+            base.brand = brand;
+            base.model = model;
+            base.office = office;
+            base.currency = currency;
+        }
+    }
+}
