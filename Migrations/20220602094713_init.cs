625ebc020843a55475164b48fed094d8988953b9
Joakim Andersson <joakim@ktv-sjobo.se>
2022-06-02 12:06:08 +0200

    Creating the model for EntityFrameworkCore

---
+++ Migrations/20220602094713_init.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Mini_Project_2.Migrations
+{
+    public partial class init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Asset",
+                columns: table => new
+                {
+                    purchaseDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    price = table.Column<int>(type: "int", nullable: false),
+                    type = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    brand = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    model = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    office = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    currency = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Asset");
+        }
+    }
+}
---
+++ Model/AssetsContext.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Mini_Project_2.Asset;
+
+namespace Mini_Project_2.Model
+{
+    public class BloggingContext : DbContext
+    {
+        public DbSet<Assets> Asset { get; set; }
+
+        /// <summary>
+        /// Connection string mssql server
+        /// </summary>
+        /// <param name="optionsBuilder"></param>
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                @"Server=localhost,1433;Database=Blogging;Trusted_Connection=False;User Id=SA;Password=@mssql1Ipw;");
+        }
+
+        /// <summary>
+        /// Table Asset set to not contain a primary key
+        /// </summary>
+        /// <param name="modelBuilder"></param>
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Assets>(
+                asset =>
+                {
+                    asset.HasNoKey();
+                });
+        }
+    }
+}
